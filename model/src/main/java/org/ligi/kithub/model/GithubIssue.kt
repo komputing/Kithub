51c8d9abdd9dfd6b46ea9d12ba35a8481d8e14ba
ligi <ligi@ligi.de>
2017-08-27 14:00:48 +0200

    Make milestone optional

--- model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
+++ model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
@@ -3,8 +3,8 @@ package org.ligi.kithub.model
 data class GithubIssue(val title: String,
                        val body: String,
                        val user: GithubUser,
-                       val milestone: String,
-                       val pullRequest: GithubPullRequest,
+                       val milestone: String?,
+                       val pullRequest: GithubPullRequest?,
                        val labels:List<String>,
                        val assignees: List<String>
 )
