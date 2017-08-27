fa85001f96602baa8f4912e31c0f37c46a61f03b
ligi <ligi@ligi.de>
2017-08-27 14:35:15 +0200

    Correct naming of PR

--- model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
+++ model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
@@ -4,7 +4,7 @@ data class GithubIssue(val title: String,
                        val body: String,
                        val user: GithubUser,
                        val milestone: String?,
-                       val pullRequest: GithubPullRequest?,
+                       val pull_request: GithubPullRequest?,
                        val labels:List<String>,
                        val assignees: List<String>
 )
