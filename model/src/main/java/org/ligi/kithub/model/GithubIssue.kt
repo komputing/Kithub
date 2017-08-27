41d89ee1aadd5895fa2383ed7e1d30090ce44832
ligi <ligi@ligi.de>
2017-08-27 13:29:58 +0200

    Attach PullRequest to issue (for kontinuum)

--- model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
+++ model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
@@ -4,6 +4,7 @@ data class GithubIssue(val title: String,
                        val body: String,
                        val user: GithubUser,
                        val milestone: String,
+                       val pullRequest: GithubPullRequest,
                        val labels:List<String>,
                        val assignees: List<String>
 )
--- model/src/main/java/org/ligi/kithub/model/GithubPullRequest.kt
+++ model/src/main/java/org/ligi/kithub/model/GithubPullRequest.kt
@@ -1,5 +1,5 @@
 package org.ligi.kithub.model
 
 data class GithubPullRequest(val url: String,
-                             val repo: GithubRepository,
+                             val repo: GithubRepository?,
                              val head: GithubCommit?)
