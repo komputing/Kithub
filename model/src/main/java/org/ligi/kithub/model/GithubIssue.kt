9f3c45fe0d2e284ba075e1b569adef309e832611
ligi <ligi@ligi.de>
2017-10-17 15:11:14 +0200

    Add issue number

--- model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
+++ model/src/main/java/org/ligi/kithub/model/GithubIssue.kt
@@ -3,6 +3,7 @@ package org.ligi.kithub.model
 data class GithubIssue(val title: String,
                        val body: String,
                        val user: GithubUser,
+                       val number: Long,
                        val milestone: String?,
                        val pull_request: GithubPullRequest?,
                        val labels:List<GithubLabel>,
